c9c698c3283533be42d96a35c2d5d4d789bf5b0d
ofcparvej <ofcparvej.mo@gmail.com>
2025-07-17 20:09:41 +0530

    jwt set in cookie in headers

--- src/main/java/com/example/auth_service/controller/AuthController.java
+++ src/main/java/com/example/auth_service/controller/AuthController.java
@@ -6,8 +6,12 @@ import com.example.auth_service.dto.PassengerDto;
 import com.example.auth_service.dto.PassengerSignupRequestDto;
 import com.example.auth_service.services.AuthService;
 import com.example.auth_service.services.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,6 +24,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    @Value("${cookie.expiry}")
+    private int cookieExpiry ;
 
     private final AuthenticationManager authenticationManager;
     private AuthService authService;
@@ -39,7 +46,7 @@ public class AuthController {
     }
 
     @PostMapping("/signin/passenger")
-    public ResponseEntity<?>signIn(@RequestBody AuthrequestDto authrequestDto){
+    public ResponseEntity<?>signIn(@RequestBody AuthrequestDto authrequestDto , HttpServletResponse response){
         System.out.println("Req---->" + authrequestDto.getEmail() + "   " + authrequestDto.getPassword());
 //        PassengerDto response = authService.signupPassenger(passengerSignupRequestDto);
         System.out.println("**********************************SignIn Controller******************************** ....");
@@ -51,8 +58,20 @@ public class AuthController {
         String jwtToken = jwtService.createToken(authrequestDto.getEmail());
 
         if(authentication.isAuthenticated()){
+            ResponseCookie cookie = ResponseCookie.from("JwtToken" , jwtToken)
+                    .httpOnly(true)
+                    .secure(false)  //to send to http also
+                    .path("/")
+                    .maxAge(cookieExpiry).build();
+
+
+            response.setHeader(HttpHeaders.SET_COOKIE, String.valueOf(cookie));
             return new ResponseEntity<>(jwtToken , HttpStatus.OK);
         }
         return new  ResponseEntity<>(" Auth Not successful ", HttpStatus.OK);
     }
 }
+
+//set response cookie in header ...
+//
+
